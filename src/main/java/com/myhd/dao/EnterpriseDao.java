8e252116f0a5a8657ef8b5094664d920cc7b3d5e
JoneElmo <mhangggggg@outlook.com>
2023-09-23 11:02:53 +0800

    完成dao层实现，除了三表联查

--- src/main/java/com/myhd/dao/EnterpriseDao.java
+++ src/main/java/com/myhd/dao/EnterpriseDao.java
@@ -1,7 +1,10 @@
 package com.myhd.dao;
 
 import com.myhd.pojo.Enterprise;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -10,9 +13,9 @@ import java.util.List;
  * packageName com.myhd.dao
  * Description TODO 根据请求返回数据给信息认证页面；无返回数据则需要接受信息认证页面传过来的数据并更新企业表；返回白名单页面或黑名单页面添加供应商时可选择的企业信息
  *
- * @author "CYQH"
+ * @author "JoneElmo"
  * @version 1.0
- * @email 1660855825@qq.com
+ * @email mhangggggg@outlook.com
  * @Date: 2023/09/22 18:12
  */
 @Mapper
@@ -21,34 +24,43 @@ public interface EnterpriseDao {
      * @description: 将信息认证页面的企业认证信息更新到企业表中。
      * @param enterprise
      * @return: java.lang.Integer
-     * @author CYQH
-     * @date: 2023/09/22 20:06
+     * @author JoneElmo
+     * @date: 2023-9-23 10:50
      */
+    @Insert("insert into enterprise(name, social_uniform_code, email, phone, address, scale, fax) \n" +
+            "values ( #{name},#{socialUniformCode},#{email},#{phone},#{address},#{scale},#{fax} );")
     Integer insertEnterprise(Enterprise enterprise);
 
     /**
      * @description: 根据查询到的用户表的企业id查询企业信息并将信息返回给信息认证页面。
      * @param enterpriseId
      * @return: com.myhd.pojo.Enterprise
-     * @author CYQH
-     * @date: 2023/09/22 20:17
+     * @author JoneElmo
+     * @date: 2023-9-23 10:50
      */
-    Enterprise selectByEnterpriseId(Integer enterpriseId);
+    @Select("select id, name, social_uniform_code, email, phone, address, scale, fax \n" +
+            "from enterprise\n" +
+            "where id = #{id}")
+    Enterprise selectByEnterpriseId(@Param("id") Integer enterpriseId);
     /**
      * @description: 查询所有企业信息（企业id、企业名称和信用代码）根据查询到的用户表里的企业id来排除查询到的白名单和黑名单供应商企业以及本企业，将查询出的数据作为可选择企业返回到白名单页面。
      * @param enterpriseId
      * @return: java.util.List<com.myhd.pojo.Enterprise>
-     * @author CYQH
-     * @date: 2023/09/22 20:30
+     * @author JoneElmo
+     * @date: 2023-9-23 10:50
      */
+
+    @Select("")
     List<Enterprise> selectEnterpriseExceptWhiteAndBlack(Integer enterpriseId);
     /**
      * @description: 查询所有企业信息（企业id、企业名称和信用代码）根据查询到的用户表里的企业id来排除查询到的黑名单供应商企业以及本企业，将查询出的数据作为可选择企业返回到黑名单页面。
      * @param enterpriseId
      * @return: java.util.List<com.myhd.pojo.Enterprise>
-     * @author CYQH
-     * @date: 2023/09/22 20:34
+     * @author JoneElmo
+     * @date: 2023-9-23 10:50
      */
+
+    @Select("")
     List<Enterprise> selectEnterpriseExceptBlack(Integer enterpriseId);
 
 
--- src/main/java/com/myhd/dao/SupplierWhiteListDao.java
+++ src/main/java/com/myhd/dao/SupplierWhiteListDao.java
@@ -2,7 +2,7 @@ package com.myhd.dao;
 
 import com.myhd.pojo.SupplierWhiteList;
 import com.myhd.pojo.ThreeTablesQuery;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -11,10 +11,10 @@ import java.util.List;
  * packageName com.myhd.dao
  * Description TODO 对白名单表进行分页查询，并将数据返回给白名单页面，将添加企业供应商的信息更新白名单表；添加黑名单时移除白名单表数据。
  *
- * @author "CYQH"
+ * @author "JoneElmo"
  * @version 1.0
- * @email 1660855825@qq.com
- * @Date: 2023/09/22 18:15
+ * @email mhangggggg@outlook.com
+ * @Date: 2023-9-23 10:50
  */
 @Mapper
 public interface SupplierWhiteListDao {
@@ -22,26 +22,33 @@ public interface SupplierWhiteListDao {
      * @description: 根据用户表查询到的企业id查询本企业的白名单表信息，再根据白名单表信息中的供应商id查询对应用户表信息和企业表信息，将数据存入到三表联查实体类中，并进行分页展示.
      * @param enterpriseId
      * @return: com.myhd.pojo.ThreeTablesQuery
-     * @author CYQH
-     * @date: 2023/09/22 20:59
+     * @author JoneElmo
+     * @date: 2023-9-23 10:50
      */
+    @Select(" ")
     List<ThreeTablesQuery>  selectWhiteInfoByEnterpriseId(Integer enterpriseId);
     /**
      * @description: 根据添加供应商获取的白名单信息更新白名单表。
      * @param supplierWhiteList
      * @return: java.lang.Integer
-     * @author CYQH
-     * @date: 2023/09/22 21:04
+     * @author JoneElmo
+     * @date: 2023-9-23 10:50
      */
+
+    @Insert("insert into supplier_white_list(enterprise_id, supplier_id, supplier_level, update_date) \n" +
+            "values (#{enterpriseId}, #{supplierId} , #{supplierLevel} , #{updateDate})")
     Integer insertWhite(SupplierWhiteList supplierWhiteList);
     /**
      * @description: 根据本企业id和选择的供应商id确定唯一一条白名单数据并移除白名单表
      * @param enterprise_id
      * @param supplier_id
      * @return: java.lang.Integer
-     * @author CYQH
-     * @date: 2023/09/22 21:13
+     * @author JoneElmo
+     * @date: 2023-9-23 10:50
      */
-    Integer deleteWhite(Integer enterprise_id,Integer supplier_id);
+
+    @Delete("delete from supplier_white_list " +
+            "where enterprise_id = #{enterpriseId} and supplier_id = #{supplierId}")
+    Integer deleteWhite(@Param("enterpriseId") Integer enterprise_id,@Param("supplierId") Integer supplier_id);
 
 }
