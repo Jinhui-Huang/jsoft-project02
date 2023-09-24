9740ff3e285c2152aec3bffeab13c70d889840d3
Jinhui-Huang <2634692718@qq.com>
2023-09-24 08:54:12 +0800

    fix: 更新EnterpriseService部分接口方法返回值为List<Enterprise>

--- src/main/java/com/myhd/service/EnterpriseService.java
+++ src/main/java/com/myhd/service/EnterpriseService.java
@@ -2,6 +2,8 @@ package com.myhd.service;
 
 import com.myhd.pojo.Enterprise;
 
+import java.util.List;
+
 /**
  * className EnterpriseService
  * packageName com.myhd.service
@@ -30,21 +32,23 @@ public interface EnterpriseService {
      * @date: 2023/09/22 20:17
      */
     Enterprise selectByEnterpriseId(Integer enterpriseId);
+
     /**
      * @description: 查询所有企业信息（企业id、企业名称和信用代码）根据查询到的用户表里的企业id来排除查询到的白名单和黑名单供应商企业以及本企业，将查询出的数据作为可选择企业返回到白名单页面。
      * @param enterpriseId
-     * @return: com.myhd.pojo.Enterprise
+     * @return: java.util.List<com.myhd.pojo.Enterprise>
      * @author CYQH
      * @date: 2023/09/22 20:30
      */
-    Enterprise selectEnterpriseExceptWhiteAndBlack(Integer enterpriseId);
+    List<Enterprise> selectEnterpriseExceptWhiteAndBlack(Integer enterpriseId);
+
     /**
      * @description: 查询所有企业信息（企业id、企业名称和信用代码）根据查询到的用户表里的企业id来排除查询到的黑名单供应商企业以及本企业，将查询出的数据作为可选择企业返回到黑名单页面。
      * @param enterpriseId
-     * @return: com.myhd.pojo.Enterprise
+     * @return: java.util.List<com.myhd.pojo.Enterprise>
      * @author CYQH
      * @date: 2023/09/22 20:34
      */
-    Enterprise selectEnterpriseExceptBlack(Integer enterpriseId);
+    List<Enterprise> selectEnterpriseExceptBlack(Integer enterpriseId);
 
 }
