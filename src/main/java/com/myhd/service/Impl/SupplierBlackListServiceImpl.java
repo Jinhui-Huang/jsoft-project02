ad996f07dc2733d851a718e1aa8268ac43b14b80
DaiYao <2385503948@qq.com>
2023-09-24 15:00:53 +0800

    refactor : 供应商黑名单的方法,抛出异常,logback记录

--- src/main/java/com/myhd/service/Impl/SupplierBlackListServiceImpl.java
+++ src/main/java/com/myhd/service/Impl/SupplierBlackListServiceImpl.java
@@ -2,11 +2,15 @@ package com.myhd.service.Impl;
 
 import com.myhd.dao.SupplierBlackListDao;
 import com.myhd.dao.SupplierWhiteListDao;
+import com.myhd.exception.BusinessException;
 import com.myhd.pojo.SelectLikeInfo;
 import com.myhd.pojo.SupplierBlackList;
 import com.myhd.pojo.ThreeTablesQuery;
 import com.myhd.service.SupplierBlackListService;
 import com.myhd.util.MyBatisUtil;
+import com.myhd.util.code.Code;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.apache.ibatis.session.SqlSession;
 
 import java.util.List;
@@ -21,6 +25,7 @@ import java.util.List;
  * @email 2385503948@qq.com
  * @Date: 2023/9/24 09:00
  */
+@Slf4j
 public class SupplierBlackListServiceImpl implements SupplierBlackListService {
     private static final SqlSession session =  MyBatisUtil.openSession(true);
     public SupplierBlackListDao sbd =  session.getMapper(SupplierBlackListDao.class);
@@ -28,18 +33,36 @@ public class SupplierBlackListServiceImpl implements SupplierBlackListService {
 
     @Override
     public List<ThreeTablesQuery> selectBlackInfoByEnterpriseId(SelectLikeInfo selectLikeInfo) {
-        return sbd.selectBlackInfoByEnterpriseId(selectLikeInfo);
+        try {
+            return sbd.selectBlackInfoByEnterpriseId(selectLikeInfo);
+        } catch (RuntimeException e) {
+            log.error(e.getMessage(),e);
+            return null;
+        }
     }
 
     @Override
     public Boolean removeBlack(Integer enterprise_id, Integer supplier_id) {
-        return sbd.deleteBlack(enterprise_id,supplier_id) == 1;
+        try {
+            return sbd.deleteBlack(enterprise_id,supplier_id) == 1;
+        } catch (RuntimeException e) {
+            log.error(e.getMessage(),e);
+            return false;
+        }
     }
 
     @Override
     public Boolean addBlack(SupplierBlackList supplierBlackList) {
-        swl.deleteWhite(supplierBlackList.getEnterpriseId(), supplierBlackList.getSupplierId());
-        Integer j = sbd.insertBlack(supplierBlackList);
-        return j==1;
+        try {
+            Integer i = swl.deleteWhite(supplierBlackList.getEnterpriseId(), supplierBlackList.getSupplierId());
+            if (i == 0){
+                throw  new BusinessException(Code.DELETE_ERR,"白名单无改条数据");
+            }
+            Integer j = sbd.insertBlack(supplierBlackList);
+            return i == 1 && j == 1 ;
+        } catch (RuntimeException e) {
+            log.error(e.getMessage(),e);
+            return false;
+        }
     }
 }
